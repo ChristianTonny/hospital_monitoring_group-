c147a2d4d3735ee63b5eccb769cdf682f9e9f425
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-13 01:00:00 +0200

    description of script

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -5,4 +5,11 @@ read -p "Enter the device name (e.g., Monitor_A): " device_name
 # Function to generate random heart rate between 40 and 120
 generate_heart_rate() {
     echo $((RANDOM % 81 + 40))
-}
+(
+    while true; do
+        timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+        heart_rate=$(generate_heart_rate)
+        echo "$timestamp $device_name $heart_rate" >> heart_rate_log.txt
+        sleep 1
+    done
+) &}
