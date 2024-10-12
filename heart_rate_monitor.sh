bf501ab7392e2952777a36320b3398c4959466f5
ChristianTonny <g.iradukund1@alustudent.com>
2024-10-12 02:32:43 -0700

    logged the randomized data into the logs

--- heart_rate_monitor.sh
+++ heart_rate_monitor.sh
@@ -6,3 +6,7 @@ read device_name
 generate_heart_rate() {
     echo $((RANDOM % 40 + 60))  # Random number between 60 and 99
 }
+
+echo "Heart rate monitoring started for $device_name. PID: $$"
+echo "Data is being logged to heart_rate_log.txt"
+echo "To stop the monitoring, use: kill $$"
