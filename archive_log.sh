c5341d18b89248ac08dcfa38a9cd4afb56da38ac
root <n.ishimwe4@alustudent.com>
2024-10-13 00:49:14 -0700

    changes

--- archive_log.sh
+++ archive_log.sh
@@ -2,7 +2,7 @@
 
 # Get the current real timestamp
 timestamp=$(date +"%Y%m%d_%H%M%S")
-# Define the log file name
+# Define the log specifaclly  file name
 log_file="heart_rate_log.txt"
 # Define the new archived file name
 archived_file="${log_file}_${timestamp}"
