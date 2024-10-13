ee0e19ac7e128480fdaf77c7f473cfe8ab62b638
root <n.ishimwe4@alustudent.com>
2024-10-13 00:54:31 -0700

    elaborate

--- archive_log.sh
+++ archive_log.sh
@@ -4,7 +4,7 @@
 timestamp=$(date +"%Y%m%d_%H%M%S")
 # Define the log specifaclly  file name
 log_file="heart_rate_log.txt"
-# Define the new archived file name
+# eleborate the new archived file name
 archived_file="${log_file}_${timestamp}"
 # Rename the log file
 if mv "$log_file" "$archived_file"; then
